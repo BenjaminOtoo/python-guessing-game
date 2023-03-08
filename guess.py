76c426323ef834c212e5d5f07a28793244a4ae12
Benjamin Otoo <116750841+BenjaminOtoo@users.noreply.github.com>
2023-03-08 17:01:05 +0000

    improved output readability

--- guess.py
+++ guess.py
@@ -8,17 +8,17 @@ def guess(x):
     while True:
         guess = int(input(f"Guess a number between 1 and {x}: "))
         if guess == random_number:
-            print("Congratulations you guessed the number!")
+            print("\nCONGRATULATIONS!! you guessed the number!")
             break
         elif tries < maxtries - 1 and guess > random_number:
             numlst.append(guess)
             tries += 1
-            print("\nGUESS LOWER\nYou have", maxtries - tries, "attempts remaining\n")
+            print("\nGUESS LOWER\nYou have", maxtries - tries, "attempts remaining")
             print("previous guesses", numlst, "\n")
         elif tries < maxtries - 1 and guess < random_number:
             numlst.append(guess)
             tries += 1
-            print("\nGUESS HIGHER\nYou have", maxtries - tries, "attempts remaining\n") 
+            print("\nGUESS HIGHER\nYou have", maxtries - tries, "attempts remaining") 
             print("previous guesses", numlst, "\n")       
         else:
             print("\nGAME OVER, You have no attempts remaining")
