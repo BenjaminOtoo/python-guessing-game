f98cbf2a2e7520a7dece24f165960f299826ebbc
Benjamin Otoo <116750841+BenjaminOtoo@users.noreply.github.com>
2023-03-08 15:01:57 +0000

    defined function for the game

---
+++ guess.py
@@ -0,0 +1,24 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    tries = 0
+    maxtries = 5
+    while guess != random_number and tries != maxtries:
+        guess = int(input(f"Guess a number between 1 and {x}: "))
+        if guess == random_number:
+            print("Congratulations you guessed the number!")
+            break
+        elif tries < 4:
+            tries += 1
+            print("Sorry try again")
+            print("You have", maxtries - tries, "attempts remaining")    
+        else:
+            print("GAME OVER, You have no attempts remaining")
+            print("The number was", random_number)
+            break    
+
+#User must input the maximum number for the range of guesses
+maxnum = int(input("Enter the maximum number: "))
+guess(maxnum)
